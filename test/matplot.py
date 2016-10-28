bae13dd584c312dfc634b92799fbf659cccd154a
Everett Kropf <ehkropf@gmail.com>
2016-10-28 14:49:33 +0100

    Start building matrix viewer.

---
+++ test/matplot.py
@@ -0,0 +1,13 @@
+#!/opt/local/bin/python
+
+from __future__ import print_function
+import numpy as np
+import matplotlib
+import matplotlib.pyplot as plt
+import os
+import sys
+
+if __name__ == "__main__":
+    # Plot script.
+
+    matname = sys.argv[1]
