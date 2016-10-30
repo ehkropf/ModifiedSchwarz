aac6bddde1cbeca92c78a713b7606cfcc7c583b3
Everett Kropf <ehkropf@gmail.com>
2016-10-30 19:14:10 +0000

    Add block L_0j. Found fundamental domain is broken.

--- firstMatrix.cpp
+++ firstMatrix.cpp
@@ -29,6 +29,44 @@ int main()
         L(arma::span(0, N-2), arma::span(Q, Q+N-2)) = arma::diagmat(tmp);
     }
 
+    // Blocks L_{0,j}.
+    for (uint j = 0; j < m; ++j)
+    {
+        if (dv(j) == 0.0)
+        {
+            std::cout << "Block L_{0," << j+1 << "} has dv_" << j+1 << " = 0." << std::endl;
+//            cvecd tmp(N);
+//            complexd logdvj = std::log(dv(j));
+//            for (uint i = 0; i < N; ++i)
+//            {
+//                tmp(i) = double(i+1)*logdvj;
+//            }
+//
+//            uint c0 = Q-1 + j*N;
+//            uint c1 = c0 + N-1; //Q-1 + j*N - 1;
+//            L(arma::span(0, N-1), arma::span(c0, c1)) = -i2pi*arma::diagmat(arma::exp(tmp));
+        }
+        else
+        {
+            std::cout << "Block L_{0," << j+1 << "} is wrt non-zero center." << std::endl;
+//            uint c0 = Q-1 + j*N;
+//
+//            cvecd tmp(N);
+//            complexd logdvj = std::log(dv(j));
+//            for (uint i = 0; i < N; ++i)
+//            {
+//                tmp(i) = double(i)*logdvj;
+//            }
+//            L(arma::span(0, N-1), c0+1) = qv(j)*arma::exp(tmp);
+//
+//            for (uint n = 3; n <= N+1; ++n)
+//            {
+//                L(arma::span(n-2, N-1), c0+n-1)
+//                    = qv(j)*L(arma::span(n-3, N-2), c0+n-2)%arma::regspace(n-2, N-1)/(n-2);
+//            }
+        }
+    }
+
     // Blocks L_{p,p}.
     for (uint p = 1; p <= m; ++p)
     {
