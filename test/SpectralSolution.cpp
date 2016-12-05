2ad3dfb9f924728ab8f57d71f22e2325e9ccad65
Everett Kropf <ehkropf@gmail.com>
2016-12-05 17:08:39 +0000

    Begin adding coefficient test.

--- test/SpectralSolution.cpp
+++ test/SpectralSolution.cpp
@@ -6,18 +6,29 @@
 
 using namespace ModifiedSchwarz;
 
-UnitCircleDomain D = domainExample3();
-
-cvecd polesInHoles(const cvecd& z)
+cvecd polesInHoles(const cvecd& z, const UnitCircleDomain& D)
 {
     cvecd w(z.n_elem, arma::fill::zeros);
-    cvecd dv = D.centers();
-    vecd qv = D.radii();
 
-    cvecd::const_iterator d = dv.begin();
-    vecd::const_iterator q = qv.begin();
-    for (; d != dv.end() && q != qv.end(); ++d, ++q)
+    cvecd::const_iterator d = D.centers().begin();
+    vecd::const_iterator q = D.radii().begin();
+    for (; d != D.centers().end() && q != D.radii().end(); ++d, ++q)
         w += *q/(z - *d)/i2pi;
 
     return w;
 }
+
+/*
+ * Compute coefficients for a solution via FFT.
+ *
+ * 1. For a truncation level of N (in the spectral formulation), we need
+ *    M = 2*N + 1 points on each boundary.
+ * 2. Get the real part of a function at M points on each boundary.
+ * 3. The "solution" on each boundary is given by the FFT of the real part.
+ */
+uint N = 16;
+uint M = 2*N + 1;
+
+UnitCircleDomain D = domainExample3();
+
+uint m = D.m();
