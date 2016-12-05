c3dbad79755c221dfbdf9fbc32a48aa4d7170ba1
Everett Kropf <ehkropf@gmail.com>
2016-12-05 14:50:52 +0000

    More efficient (?) allocation for polynomial value.

--- src/ModifiedSchwarz.cpp
+++ src/ModifiedSchwarz.cpp
@@ -8,7 +8,7 @@ namespace ModifiedSchwarz
 ////////////////////////////////////////////////////////////////////////
 cvecd polyval(const cvecd& a, const cvecd& x)
 {
-    cvecd pn = 0.*x;
+    cvecd pn(x.n_elem, arma::fill::zeros);
     for (cmatd::const_iterator i = a.begin(); i != a.end(); ++i)
         pn = pn%x + *i;
 
