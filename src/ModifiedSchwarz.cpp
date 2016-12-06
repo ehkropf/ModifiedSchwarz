98a1fefb1ed9d0ac96c21f9ccac017992674da53
Everett Kropf <ehkropf@gmail.com>
2016-12-06 17:57:47 +0000

    Not crashing eval.

--- src/ModifiedSchwarz.cpp
+++ src/ModifiedSchwarz.cpp
@@ -194,13 +194,25 @@ SpectralData::constructMatrix(uint truncation)
 cvecd
 SpectralSolution::eval(const cvecd& z)
 {
-    cvecd w(z.n_elem);
-    cmatd& a = _coefficients;
-    unsigned N = (a.n_rows - 1)/2;
-    const UnitCircleDomain& D = _domainData->domain();
+    using namespace arma;
 
+    const UnitCircleDomain& D = _domainData->domain();
+    const cvecd& dv = D.centers();
+    const vecd& qv = D.radii();
+    const cmatd& a = _coefficients;
+    unsigned N = (a.n_rows - 1)/2;
+
+    cvecd w(z.n_elem);
     for (unsigned j = 0; j < D.m()+1; ++j)
     {
+        const ComplexDouble dj = j > 0 ? dv(j-1) : 0.;
+        const double qj = j > 0 ? qv(j-1) : 1.;
+
+        cvecd zj = z - dj;
+        uvec L = find(abs(qj - abs(zj)) < eps2pi);
+        if (L.n_elem)
+            w.elem(L) = a(0, j) +
+                2.*real(polyval(join_vert(flipud(a.col(j).subvec(1, N)), cvecd(0.)), zj(L)/qj));
     }
 
     return w;
