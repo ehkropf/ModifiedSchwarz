d066d1e9456905095017f554bd36110e72be0f4a
Everett Kropf <ehkropf@gmail.com>
2016-12-09 12:47:46 +0000

    Evaluation check passes.

--- test/checkInterpolant.cpp
+++ test/checkInterpolant.cpp
@@ -6,13 +6,13 @@
 
 using namespace ModifiedSchwarz;
 
-cx_mat polesInHoles(const cx_mat& z, const UnitCircleDomain& D)
+cx_mat polesInHolesRI(const cx_mat& z, const UnitCircleDomain& D)
 {
     cx_mat w(size(z), arma::fill::zeros);
 
-    auto d = D.centers().begin();
-    auto q = D.radii().begin();
-    for (; d != D.centers().end() && q != D.radii().end(); ++d, ++q)
+    auto d = D.centers().begin(), de = D.centers().end();
+    auto q = D.radii().begin(), qe = D.radii().end();
+    for (; d != de && q != qe; ++d, ++q)
         w += *q/(z - *d)/i2pi;
 
     return w;
@@ -20,13 +20,13 @@ cx_mat polesInHoles(const cx_mat& z, const UnitCircleDomain& D)
 
 TEST(BasicInterp)
 {
-    unsigned N = 32;
+    unsigned N = 128;
     auto D = domainExample3();
     auto zb = D.boundaryPoints(N);
 
     // Real part of sample function.
-    auto g = [&D](const cx_mat& z) -> mat { return real(polesInHoles(z, D)); };
+    auto g = [&D](const cx_mat& z) -> mat { return real(polesInHolesRI(z, D)); };
 
     RealInterpolant gi(D, g(zb));
-    CHECK(approx_equal(gi.eval(zb), g(zb), "absdiff", eps2pi));
+    CHECK(approx_equal(gi.eval(vectorise(zb)), real(vectorise(g(zb))), "absdiff", eps2pi));
 }
