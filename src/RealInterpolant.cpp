00c10599ada33e0336dd33387dd14e84834e1255
Everett Kropf <ehkropf@gmail.com>
2016-12-09 12:42:00 +0000

    Construction and eval sorted.

--- src/RealInterpolant.cpp
+++ src/RealInterpolant.cpp
@@ -7,18 +7,32 @@ namespace ModifiedSchwarz
 void
 RealInterpolant::prepareInterpolant()
 {
-    unsigned N = _boundaryData.n_rows;
-    cx_mat c(fft(_boundaryData)/N);
+    unsigned M = _boundaryData.n_rows;
+    unsigned N = (unsigned)std::ceil((M - 1)/2.);
+    cx_mat c(fft(_boundaryData)/M);
     _constants = real(c.row(0).st());
-    _coefficients = flipud(c.rows(1, N));
+    _coefficients = join_vert(flipud(c.rows(1, N)),
+                              cx_mat(1, _boundaryData.n_cols, arma::fill::zeros));
 }
 
 ////////////////////////////////////////////////////////////////////////
-cx_vec
+colvec
 RealInterpolant::eval(const cx_vec& z) const
 {
-    cx_vec w(size(z));
-    w.fill(arma::datum::nan);
+    using namespace arma;
+
+    unsigned m = _domain.m();
+    colvec w(size(z));
+    w.fill(datum::nan);
+    for (unsigned j = 0; j < m+1; ++j)
+    {
+        uvec L = _domain.isOnC(j, z);
+        if (L.n_elem)
+            w.elem(L) = _constants(j)
+                    + real(2.*polyval(_coefficients.col(j),
+                                      (z(L) - _domain.dv0(j))/_domain.qv0(j)));
+    }
+
 
     return w;
 }
