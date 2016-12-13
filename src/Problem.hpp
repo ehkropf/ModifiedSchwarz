50941c8f657804b2a4c07bd35f2bff9d6520220b
Everett Kropf <ehkropf@gmail.com>
2016-12-13 18:47:19 +0000

    Problem now ONLY encapsulates data.

--- src/Problem.hpp
+++ src/Problem.hpp
@@ -1,10 +1,8 @@
 #ifndef SCHWARZPROBLEM_HPP
 #define SCHWARZPROBLEM_HPP
 
-#include <memory>
-
-#include "UnitCircleDomain.hpp"
 #include "SchwarzTypes.hpp"
+#include "UnitCircleDomain.hpp"
 
 namespace ModifiedSchwarz
 {
@@ -20,23 +18,17 @@ namespace ModifiedSchwarz
  * this is highly dependent on the solver.
  * Of course this means we expect the Solver to conform to this interface.
  */
-template<class Solver, class Solution>
 class Problem
 {
     UnitCircleDomain _domain;
     cx_mat _boundaryData;
-    Solver _solver;
 
 public:
-    Problem(UnitCircleDomain domain, cx_mat boundaryData, Solver solver)
-        : _domain(domain), _boundaryData(boundaryData), _solver(solver) {}
+    Problem(UnitCircleDomain domain, cx_mat boundaryData)
+        : _domain(domain), _boundaryData(boundaryData) {}
 
     const UnitCircleDomain& domain() const { return _domain; }
     const cx_mat& data() const { return _boundaryData; }
-    const Solver& solver() const { return _solver; }
-
-    Solution solve() { return _solver.solve(*this); }
-    Solution solve(const Solution& prevSolution) { return _solver.solve(*this, prevSolution); }
 };
 
 }; // namespace ModifiedSchwarz
