ca72265a3ba99e29e8174151bbd75bdaf288bb5a
Everett Kropf <ehkropf@gmail.com>
2016-12-21 13:52:04 +0000

    Flesh out check.

--- test/checkSpectralMethod.cpp
+++ test/checkSpectralMethod.cpp
@@ -4,7 +4,7 @@
 #include "SpectralMethod.hpp"
 #include "UnitCircleDomain.hpp"
 
-cx_vec polesInHoles(const cx_vec&, const UnitCircleDomain&);
+#include "TestShared.hpp"
 
 TEST(theRHS)
 {
@@ -12,6 +12,16 @@ TEST(theRHS)
     using namespace arma;
 
     auto D = domainExample3();
-
     auto g = [&D](const cx_vec& z) -> colvec { return real(polesInHoles(z, D)); };
+
+    mat imagPart = reshape(g(vectorise(D.boundaryPoints(200))), 200, D.m()+1);
+    SpectralMethod method(Problem(D, imagPart));
+    cx_vec rhs = method.computeRHS(100);
+
+    cx_vec refRHS;
+    CHECK(refRHS.load("../test/refRHS.dat"));
+
+    std::cout << join_horiz(rhs, refRHS);
+
+    CHECK(approx_equal(rhs, refRHS, "absdiff", eps2pi));
 }
