f164df235c8b6268b144ef78f4ec18eac4638ef2
Everett Kropf <ehkropf@gmail.com>
2016-12-22 18:11:58 +0000

    Match reference generator function.

--- test/TestShared.cpp
+++ test/TestShared.cpp
@@ -7,10 +7,8 @@ cx_vec polesInHoles(const cx_vec& z, const UnitCircleDomain& D)
 {
     cx_vec w(z.n_elem, arma::fill::zeros);
 
-    auto d = D.centers().begin();
-    auto q = D.radii().begin();
-    for (; d != D.centers().end() && q != D.radii().end(); ++d, ++q)
-        w += *q/(z - *d)/i2pi;
+    for (auto d = D.centers().begin(); d != D.centers().end(); ++d)
+        w += 1./(z - *d);
 
     return w;
 }
