29989a16091569115c1a4e6ede89511b54d8553f
Everett Kropf <ehkropf@gmail.com>
2016-12-23 09:43:23 +0000

    Add matrix output target.

--- test/matWrite.cpp
+++ test/matWrite.cpp
@@ -4,11 +4,7 @@ int main()
 {
     using namespace ModifiedSchwarz;
 
-    // Spectral truncation level.
-    unsigned N = 64;
-
-    SpectralData smat(domainExample3(), N);
-
+    SpectralData smat(domainExample3(), 64);
     const cx_mat& L = smat.matrix();
     L.save("Lmatrix.dat", arma::arma_ascii);
 
