4188793dfffe9b8be011e7224ee977dc18f9bb62
Everett Kropf <ehkropf@gmail.com>
2016-12-23 09:43:34 +0000

    Add matrix reference data check.

--- test/checkSpectralData.cpp
+++ test/checkSpectralData.cpp
@@ -7,6 +7,17 @@
 
 using namespace ModifiedSchwarz;
 
+TEST(Matrix)
+{
+    SpectralData data(domainExample3(), 64);
+    const cx_mat& L = data.matrix();
+
+    cx_mat refL;
+    refL.load("../test/refmat.dat");
+
+    CHECK(arma::approx_equal(L, refL, "absdiff", 1e-4));
+}
+
 TEST(Sharing)
 {
     SpectralData data(domainExample3());
