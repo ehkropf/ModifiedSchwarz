d50b2b9e23f8d7650251cde529d9f052b0015b99
Everett Kropf <ehkropf@gmail.com>
2017-01-08 15:33:52 +0000

    Simplify with for-range use. Add doc comment.

--- src/Utility.cpp
+++ src/Utility.cpp
@@ -7,8 +7,7 @@ namespace ModifiedSchwarz
 cx_vec polyval(const cx_vec& a, const cx_vec& x)
 {
     cx_vec pn(x.n_elem, arma::fill::zeros);
-    for (cx_mat::const_iterator i = a.begin(); i != a.end(); ++i)
-        pn = pn%x + *i;
+    for (const cx_double& ai : a) pn = pn%x + ai;
 
     return pn;
 }
