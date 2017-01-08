67af21ecc6bdff89d6d7a01951e9b20809cf6d96
Everett Kropf <ehkropf@gmail.com>
2017-01-08 18:01:19 +0000

    Use shared test function.

--- test/checkInterpolant.cpp
+++ test/checkInterpolant.cpp
@@ -3,20 +3,9 @@
 #include "SchwarzTypes.hpp"
 #include "UnitCircleDomain.cpp"
 #include "RealInterpolant.hpp"
+#include "TestShared.hpp"
 
 using namespace ModifiedSchwarz;
-
-cx_mat polesInHolesRI(const cx_mat& z, const UnitCircleDomain& D)
-{
-    cx_mat w(size(z), arma::fill::zeros);
-
-    auto d = D.centers().begin(), de = D.centers().end();
-    auto q = D.radii().begin(), qe = D.radii().end();
-    for (; d != de && q != qe; ++d, ++q)
-        w += *q/(z - *d)/i2pi;
-
-    return w;
-}
 
 TEST(BasicInterp)
 {
@@ -25,8 +14,8 @@ TEST(BasicInterp)
     auto zb = D.boundaryPoints(N);
 
     // Real part of sample function.
-    auto g = [&D](const cx_mat& z) -> mat { return real(polesInHolesRI(z, D)); };
+    auto g = [&D](const cx_mat& z) -> mat { return real(polesInHoles(z, D)); };
 
     RealInterpolant gi(D, g(zb));
-    CHECK(approx_equal(gi.eval(vectorise(zb)), real(vectorise(g(zb))), "absdiff", eps2pi));
+    CHECK(approx_equal(gi.eval(vectorise(zb)), real(vectorise(g(zb))), "absdiff", 10.*eps2pi));
 }
