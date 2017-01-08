4ebd8ce459d2acf77987f16d30d31f99dd1832a9
Everett Kropf <ehkropf@gmail.com>
2017-01-08 18:04:57 +0000

    Change template type name.

--- test/TestShared.hpp
+++ test/TestShared.hpp
@@ -7,10 +7,10 @@
 namespace ModifiedSchwarz
 {
 
-template<typename MatrixType>
-MatrixType polesInHoles(const MatrixType& z, const UnitCircleDomain& D)
+template<typename ArmaMatLike>
+ArmaMatLike polesInHoles(const ArmaMatLike& z, const UnitCircleDomain& D)
 {
-    MatrixType w(size(z), arma::fill::zeros);
+    ArmaMatLike w(size(z), arma::fill::zeros);
     for (auto & d : D.centers()) w += 1./(z - d);
 
     return w;
