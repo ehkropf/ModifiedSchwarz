1f05e9378608d3a7e485dbbdb610481927a64777
Everett Kropf <ehkropf@gmail.com>
2017-01-13 10:24:47 +0000

    Add linear system residual check.

--- test/checkSpectralMethod.cpp
+++ test/checkSpectralMethod.cpp
@@ -6,14 +6,18 @@
 
 #include "TestShared.hpp"
 
+using namespace ModifiedSchwarz;
+using namespace arma;
+
+static auto D = domainExample3();
+//static auto g = [D](const cx_vec& z) -> colvec { return imag(polesInHoles(z, D)); };
+colvec g(const cx_vec& z)
+{
+    return imag(polesInHoles(z, D));
+}
+
 TEST(theRHS)
 {
-    using namespace ModifiedSchwarz;
-    using namespace arma;
-
-    auto D = domainExample3();
-    auto g = [&D](const cx_vec& z) -> colvec { return imag(polesInHoles(z, D)); };
-
     mat imagPart = reshape(g(vectorise(D.boundaryPoints(200))), 200, D.m()+1);
     SpectralMethod method(Problem(D, imagPart));
     cx_vec rhs = method.computeRHS(100);
@@ -24,5 +28,18 @@ TEST(theRHS)
 //    colvec abserr = abs(refRHS - rhs);
 //    std::cout << " max absolute error: " << max(abserr) << std::endl;
 
-    CHECK(approx_equal(rhs, refRHS, "absdiff", 1e-4));
+    CHECK(approx_equal(rhs.head_rows(rhs.n_rows/2), refRHS, "absdiff", 1e-4));
+}
+
+TEST(residual)
+{
+    mat imagPart = reshape(g(vectorise(D.boundaryPoints(200))), 200, D.m()+1);
+    SpectralMethod method(Problem(D, imagPart));
+
+    cx_vec rhs = method.computeRHS();
+    const cx_mat& A = method.matrix();
+    cx_vec x = solve(A, rhs);
+
+    cx_vec res = rhs - A*x;
+    CHECK(max(abs(res)/abs(rhs)) < 100*eps2pi);
 }
