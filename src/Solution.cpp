37556f0f2711cb4e2f36918aa358c8c5b2231c20
Everett Kropf <ehkropf@gmail.com>
2017-01-17 18:22:59 +0000

    Begin filling in eval code.

--- src/Solution.cpp
+++ src/Solution.cpp
@@ -15,4 +15,14 @@ Solution::Solution(RealInterpolant realPart, colvec constants, RealInterpolant i
           _imagPart(imagPart),
           _pSolverData(pSolverData) {}
 
+//////////////////////////////////////////////////////////////////////////////
+cx_vec Solution::eval(const cx_vec& z) const
+{
+    // Separate boundary and domain points.
+    // Boundary points are given by realPart(z_j) + 1i*(constant_j + imagPart(z_j)).
+    // Domain values are given by barycentric interpolant.
+
+    return cx_vec();
+}
+
 }; // namespace ModifiedSchwarz
