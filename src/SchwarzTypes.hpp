e159be70854d908b78a62a982caf9533c5f42934
Everett Kropf <ehkropf@gmail.com>
2017-01-19 11:00:58 +0000

    Add doc comments.

--- src/SchwarzTypes.hpp
+++ src/SchwarzTypes.hpp
@@ -7,17 +7,22 @@
 namespace ModifiedSchwarz
 {
 
+//! Column vector of unsigned integers.
 using uvec = arma::uvec;
-using mat = arma::mat;
-using cx_double = arma::cx_double;
-using cx_vec = arma::cx_vec;
-using cx_mat = arma::cx_mat;
+//! Column vector of doubles.
 using colvec = arma::colvec;
+//! Matrix of doubles.
+using mat = arma::mat;
+//! Complex double.
+using cx_double = arma::cx_double;
+//! Column vector of complex doubles.
+using cx_vec = arma::cx_vec;
+//! Matrix of complex doubles.
+using cx_mat = arma::cx_mat;
 
 const double pi = std::acos(-1);
 const cx_double i2pi(0.0, 2.0*pi);
 const double eps2pi = std::numeric_limits<double>::epsilon()*2.*pi;
-
 
 }; // namespace ModifiedSchwarz
 
