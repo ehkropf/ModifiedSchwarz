299cdf11b630b18ed0f9102b996d10f2209910b5
Everett Kropf <ehkropf@gmail.com>
2017-03-02 14:42:36 +0000

    Add solution class check.

---
+++ test/checkSolution.cpp
@@ -0,0 +1,42 @@
+#include "UnitTest++/UnitTest++.h"
+
+#include "Solution.hpp"
+#include "UnitCircleDomain.hpp"
+#include "TestShared.hpp"
+
+using namespace ModifiedSchwarz;
+
+SUITE(SolutionTest)
+{
+
+class TestFixture
+{
+    public:
+    UnitCircleDomain domain;
+    RealInterpolant realPart;
+    RealInterpolant imagPart;
+
+    static constexpr unsigned npts = 200;
+
+    TestFixture() : domain(domainExample3())
+    {
+        cx_mat zb = domain.boundaryPoints(npts);
+
+        realPart = RealInterpolant(domain, real(polesInHoles(zb, domain)));
+        imagPart = RealInterpolant(domain, imag(polesInHoles(zb, domain)));
+    }
+};
+
+TEST_FIXTURE(TestFixture, EvalSolution)
+{
+    Solution sol(realPart, colvec(domain.m()+1, arma::fill::zeros), imagPart);
+
+    cx_mat zb = domain.boundaryPoints(10);
+    CHECK(approx_equal(polesInHoles(cx_vec(vectorise(zb)), domain), sol(vectorise(zb)), "reldiff", 10*eps2pi));
+}
+
+TEST(StoreData)
+{
+}
+
+}
