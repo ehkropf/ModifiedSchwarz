b3bb9711a7dcdc988baa7761aace953b1a8e82ce
Everett Kropf <ehkropf@gmail.com>
2017-03-05 15:25:24 +0000

    Finalise first problem check.

--- test/checkProblem.cpp
+++ test/checkProblem.cpp
@@ -6,11 +6,10 @@
 
 using namespace ModifiedSchwarz;
 
-TEST(CallSolve)
+TEST(BasicSolve)
 {
     unsigned N = 128;
     auto D = domainExample3();
-    //auto zb = D.boundaryPoints(N);
     auto g = [&D](const cx_mat& z) -> mat { return imag(polesInHoles(z, D)); };
 
     Problem problem(RealInterpolant(D, g(D.boundaryPoints(N))));
@@ -19,14 +18,5 @@ TEST(CallSolve)
     cx_vec zt = vectorise(D.boundaryPoints(7));
     cx_vec actual = polesInHoles(zt, D);
 
-//    std::cout << "Imag part check:\n"
-//        << imag(actual) - sol.imagPart()(zt) << std::endl;
-//
-//    std::cout << "Real part check:\n"
-//        << join_horiz(real(actual), sol.realPart()(zt)) << std::endl;
-//
-//    cx_vec err = polesInHoles(zt, D) - sol(zt);
-//    std::cout << "max(abs(err) = " << max(abs(err)) << std::endl;
-
-//    CHECK(false);
+    CHECK(arma::approx_equal(polesInHoles(zt, D), sol(zt), "reldiff", 1e-6));
 }
